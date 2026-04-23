25f6b6b94362781d8d3c1cb3b14cc5c970a4ab55
Tom Softreck <tom@sapletta.com>
2026-04-23 14:59:08 +0200

    refactoring

--- godot/output/structs.go
+++ godot/output/structs.go
@@ -1,13 +1,15 @@
 
 // structs.go - generated from JSON
+package main
+
 type ViewBundle struct {
-	Bundle     string     `json:"bundle"`
-	Kind       string     `json:"kind"`
-	Version    string     `json:"version"`
-	Description string    `json:"description"`
-	Sources    []Source   `json:"sources"`
-	Template   Template   `json:"template"`
-	Output     Output     `json:"output"`
+	Bundle      string     `json:"bundle"`
+	Kind        string     `json:"kind"`
+	Version     string     `json:"version"`
+	Description string     `json:"description"`
+	Sources     []Source   `json:"sources"`
+	Template    Template   `json:"template"`
+	Output      Output     `json:"output"`
 }
 
 type Source struct {
@@ -16,4 +18,15 @@ type Source struct {
 	Refresh   string   `json:"refresh"`
 	Type      string   `json:"type"`
 	DependsOn []string `json:"depends_on,omitempty"`
+}
+
+type Template struct {
+	Engine    string `json:"engine"`
+	SourceURI string `json:"source_uri"`
+}
+
+type Output struct {
+	Format   string `json:"format"`
+	Runtime  string `json:"runtime"`
+	Port     int    `json:"port"`
 }
